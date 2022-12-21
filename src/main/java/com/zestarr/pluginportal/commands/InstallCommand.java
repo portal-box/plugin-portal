1ab08326223e9701e2c26c3cf83bc64e5403fb23
Zestarr <zestarpersonal@gmail.com>
2022-12-21 16:30:52 -0500

    random features

--- src/main/java/com/zestarr/pluginportal/commands/InstallCommand.java
+++ src/main/java/com/zestarr/pluginportal/commands/InstallCommand.java
@@ -3,11 +3,17 @@ package com.zestarr.pluginportal.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import com.zestarr.pluginportal.PluginPortal;
+import com.zestarr.pluginportal.types.Plugin;
 import com.zestarr.pluginportal.utils.HttpUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.File;
 import java.net.http.HttpClient;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.zestarr.pluginportal.PluginPortal.getPluginManager;
 import static com.zestarr.pluginportal.utils.ConfigUtils.getPluginFolder;
@@ -21,7 +27,28 @@ public class InstallCommand extends BaseCommand {
     public boolean onCommand(CommandSender sender, String pluginKey) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            HttpUtils.download(getPluginManager().getPlugins().get(pluginKey), getPluginFolder());
+            // Check if plugin is valid
+            Map<String, Plugin> plugins = getPluginManager().getPlugins();
+            if (plugins.get(pluginKey) == null) {
+                player.sendMessage(ChatColor.RED + "Plugin not found! Plugin List: ");
+
+                for (Plugin plugin : plugins.values()) {
+                    player.sendMessage(ChatColor.GREEN + " + " + ChatColor.GRAY + plugin.getDisplayName());
+                }
+
+                return true;
+            } else {
+                if (new File(getPluginFolder(), pluginKey + ".jar").exists()) { // Broken, Method is to save all saved plugins "display name" and the file name to a hashmap/file and check from there
+                    player.sendMessage(ChatColor.RED + "Plugin already installed!");
+                    return true;
+                } else {
+                    HttpUtils.download(getPluginManager().getPlugins().get(pluginKey), getPluginFolder());
+                }
+            }
+
+
+
+
         }
         return true;
     }
--- src/main/java/com/zestarr/pluginportal/types/Plugin.java
+++ src/main/java/com/zestarr/pluginportal/types/Plugin.java
@@ -5,11 +5,13 @@ import lombok.Data;
 @Data
 public class Plugin {
 
-    private String fileName;
+    private String fileName; // Optional
     private String displayName;
     private String version;
     private String description;
     private String downloadLink;
     private String sha256;
+    private String[] dependencies; // Optional
+    private String[] recommendedPlugins; // Optional
 
 }
